6faefb47237a64457f7dd6a386f80b0310888c92
Preeti <singhp1992@gmail.com>
2020-08-18 17:13:55 +0200

    refactor: renaming last HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to my website"/>
+    <Home msg="Welcome to my website"/>
   </div>
 </template>
 
